64a9c49a601dc9c9b99bea5a6e4e09a2b5c1f57f
Cao Tung Duong <tungduong@Caos-MacBook-Pro.local>
2025-08-22 10:54:36 +0700

    add space at end of incline and change position of noice

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -16,7 +16,7 @@ return {
 			},
 			presets = {
 				bottom_search = true, -- use a classic bottom cmdline for search
-				-- command_palette = true, -- position the cmdline and popupmenu together
+				command_palette = true, -- position the cmdline and popupmenu together
 				long_message_to_split = true, -- long messages will be sent to a split
 				inc_rename = false, -- enables an input dialog for inc-rename.nvim
 				lsp_doc_border = true, -- add a border to hover docs and signature help
@@ -32,7 +32,7 @@ return {
 			timeout = 200,
 			stages = "fade",
 			background_colour = "#0000000",
-			top_down = false,
+			-- top_down = false,
 		})
 	end,
 }
